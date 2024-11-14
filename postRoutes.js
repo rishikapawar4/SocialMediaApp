687ebe6cfb0abc4e07be9ee027f11f5739235e72
Rishika Pawar <122422977+rishikapawar4@users.noreply.github.com>
2024-11-14 19:54:09 +0530

    Delete postRoutes.js

--- postRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createPost } = require('../controllers/postController');
-const authmiddleware = require('../utils/authmiddleware');
-const router = express.Router();
-
-router.post('/create', authmiddleware, createPost);
-
-module.exports = router;
